8678408a762b9ab89c21822354642cf075864837
NgojcSown <107361006+NgojcSown@users.noreply.github.com>
2023-02-28 03:59:24 +0700

    build input search

--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -1,14 +1,31 @@
 import styles from './Header.module.scss';
 import classNames from 'classnames/bind';
-
+import images from '~/assets/images';
 
 const cx = classNames.bind(styles)
-
 function Header() {
     return (
         <header className={cx('wrapper')}>
             <div className={cx('content')}>
+                <div className={cx('logo)')}>
+                    <img src={images.logo.default} alt='tik tok' />
 
+                </div>
+                <div className={cx('search')}>
+                    <input placeholder='Search accounts and videos' alt='search' spellCheck={false} />
+                    <button className={cx('clear')}>
+                        <i className={cx('fa-regular fa-circle-xmark')}></i>
+                    </button>
+                    <div className={cx('loading')}>
+                        <i className={cx('fa-solid fa-spinner')}></i>
+                    </div>
+                    <button className={cx('search-btn')} >
+                        <i className={cx("fa-solid fa-magnifying-glass")}></i>
+                    </button>
+                </div>
+                <div className={cx('actions')}>
+
+                </div>
             </div>
         </header>);
 }
